79c37124f4cf0152b155ed73cef0ec98838889bc
miomao34 <flex384@gmail.com>
2019-10-06 22:31:51 +0300

    second task ready

--- task02/series.pas
+++ task02/series.pas
@@ -7,23 +7,33 @@ uses
 	SysUtils,
 	Math;
 var
+	real_result : double = 1.0;
 	result : double = 1.0;
-	newresult : double = 1.0;
 	n : integer = 1;
 	x : double = 0.0;
-	error : double = 0.0;
+	error : double = 1.1;
 begin
+	writeln('cos(x): enter x and error');
+	{$I-}
 	readln(x, error);
-	// x := x * pi / 180;
+	{$I+}
+
+	if (ioresult <> 0) or (error < 0.0000000001)then
+	begin
+		writeln('wrong input');
+		exit;
+	end;
+
+	real_result := cos(x*pi/180);
+
 	while true do
 	begin
-		newresult := result * (1 - ( 4*x*x/((2*n-1)*(2*n-1)*180*180) ) );
-		if ((newresult - result) < error) and ((result - newresult) < error) then
+		result := result * (1 - ( 4*x*x/((2*n-1)*(2*n-1)*180*180) ) );
+		if ((result - real_result) < error) and ((real_result - result) < error) then
 		begin
-			writeln(newresult);
+			writeln(result:0:20);
 			break;
 		end;
-		result := newresult;
 		n := n + 1;
 	end;
 end.
